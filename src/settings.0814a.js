85d01ee414e37218afc385e8b32830655a9a9efc
Thinh Nguyen <ngocthinh2408@gmail.com>
2024-06-10 22:00:18 +0700

    sfs

---
+++ src/settings.0814a.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/SceneLoading.fire",orientation:"landscape",jsList:["assets/Script/crypto-js/crypto-js.de9cc.js"],bundleVers:{internal:"6c0a3",resources:"48b68",main:"6c2b0"}};
